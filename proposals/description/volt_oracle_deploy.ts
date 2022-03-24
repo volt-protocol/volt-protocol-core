68f8a58b22a4753fd941343a1787530d2ba313f7
Elliot <elliotfriedman3@gmail.com>
2022-03-23 17:53:03 -0700

    remove integration testing and DAO framework

--- proposals/description/volt_oracle_deploy.ts
+++
@@ -1,9 +0,0 @@
-import { ProposalDescription } from '@custom-types/types';
-
-const volt_oracle_deploy: ProposalDescription = {
-  title: 'FIP-Deploy: Create Volt',
-  commands: [],
-  description: ``
-};
-
-export default volt_oracle_deploy;
