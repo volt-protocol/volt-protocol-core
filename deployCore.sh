2c0bc1740173111445d582eab215dd1f71c2edc5
Elliot <elliotfriedman3@gmail.com>
2022-03-25 11:50:36 -0700

    gas optimization

--- deployCore.sh
+++ deployCore.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-RPC_URL=http://127.0.0.1:8545/
-PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+RPC_URL= # e.g. http://127.0.0.1:8545/
+PRIVATE_KEY=
 
 # Global Rate Limited Minter Params
 GLOBAL_MAX_RATE_LIMIT_PER_SECOND=100000000000000000000000
