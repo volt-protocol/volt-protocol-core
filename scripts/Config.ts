fa213de82ef7cc221ac8b29145d1f6df36e60852
Elliot <elliotfriedman3@gmail.com>
2022-04-15 18:16:30 -0700

    update starting price to FPI price in ScalingPriceOracle

--- scripts/Config.ts
+++ scripts/Config.ts
@@ -1,18 +1,18 @@
 import { ethers } from 'ethers';
 
 const config = {
-  MAINNET_DEPLOYMENT: false,
+  MAINNET_DEPLOYMENT: true,
   CHAINLINK_FEE: ethers.utils.parseEther('10'),
   CHAINLINK_ORACLE_ADDRESS: '0x049bd8c3adc3fe7d3fc2a44541d955a537c2a484',
-  ORACLE_PASS_THROUGH_ADDRESS: '', // TODO fill this in once deployed
-  SCALING_PRICE_ORACLE_ADDRESS: '', // TODO fill this in once deployed
+  ORACLE_PASS_THROUGH_ADDRESS: '0x84dc71500D504163A87756dB6368CC8bB654592f',
+  SCALING_PRICE_ORACLE_ADDRESS: '0x79412660E95F94a4D2d02a050CEA776200939917',
   MULTISIG_ADDRESS: '0x016177eDbB6809338Fda77b493cA01EA6D7Fc0D4',
   JOB_ID: ethers.utils.toUtf8Bytes('6f7fb4abcedb485ab27eb7bb39caf827'),
   CURRENT_MONTH_INFLATION_DATA: '287504', // March 2022 Inflation Data
   PREVIOUS_MONTH_INFLATION_DATA: '283716', // February 2022 Inflation Data
   MINT_FEE_BASIS_POINTS: 10, // Fee tentatively set at 10 basis points
   REDEEM_FEE_BASIS_POINTS: 0,
-  DEPLOYER_VOLT_AMOUNT: ethers.utils.parseEther('42000000'), // 42m
+  DEPLOYER_VOLT_AMOUNT: ethers.utils.parseEther('40000000'), // 40m
   MAX_BUFFER_CAP: ethers.utils.parseEther('10000000'), // 10m
   MAX_BUFFER_CAP_MULTI_RATE_LIMITED: ethers.utils.parseEther('100000000'), // 100m
   RATE_LIMIT_PER_SECOND: ethers.utils.parseEther('10000'), // 10k VOLT/s
