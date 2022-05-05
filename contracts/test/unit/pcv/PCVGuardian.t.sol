3c7b37693fa19efb481efba3eef9428be5d1249a
kassim <alkassimk@gmail.com>
2022-05-05 09:29:25 +0100

    add unit tests

--- contracts/test/unit/pcv/PCVGuardian.t.sol
+++ contracts/test/unit/pcv/PCVGuardian.t.sol
@@ -5,8 +5,8 @@ import {PCVGuardian} from "../../../pcv/PCVGuardian.sol";
 import {MockERC20} from "../../../mock/MockERC20.sol";
 import {MockPCVDepositV2} from "../../../mock/MockPCVDepositV2.sol";
 import {getCore, getAddresses, FeiTestAddresses} from "./../utils/Fixtures.sol";
+import {TribeRoles} from "../../../core/TribeRoles.sol";
 import {ICore} from "../../../core/ICore.sol";
-
 import {DSTest} from "./../utils/DSTest.sol";
 import {Vm} from "./../utils/Vm.sol";
 
@@ -16,12 +16,13 @@ contract PCVGuardianTest is DSTest {
     MockPCVDepositV2 public pcvDeposit;
     ICore private core;
 
-    uint256 public mintAmount = 10_000_000;
-
     Vm public constant vm = Vm(HEVM_ADDRESS);
     FeiTestAddresses public addresses = getAddresses();
 
     address[] public whitelistAddresses;
+    address public guard = address(0x123456789);
+
+    uint256 public mintAmount = 10_000_000;
 
     function setUp() public {
         core = getCore();
@@ -39,7 +40,7 @@ contract PCVGuardianTest is DSTest {
 
         pcvGuardian = new PCVGuardian(
             address(core),
-            address(this),
+            address(this), // using 'this' address as the safe address for withdrawals
             whitelistAddresses
         );
 
@@ -47,6 +48,10 @@ contract PCVGuardianTest is DSTest {
         vm.startPrank(addresses.governorAddress);
         core.grantPCVController(address(pcvGuardian));
         core.grantGuardian(address(pcvGuardian));
+
+        // create the PCV guard role, and grant it to the 'guard' address
+        core.createRole(TribeRoles.PCV_GUARD, TribeRoles.GOVERNOR);
+        core.grantRole(TribeRoles.PCV_GUARD, guard);
 
         underlyingToken.mint(address(pcvDeposit), mintAmount);
         pcvDeposit.deposit();
@@ -58,23 +63,50 @@ contract PCVGuardianTest is DSTest {
         assert(core.isPCVController(address(pcvGuardian)));
     }
 
-    function testPCVWithdrawToSafeAddress() public {
-        vm.startPrank(addresses.governorAddress);
-
+    function testWithdrawToSafeAddress() public {
+        vm.prank(addresses.governorAddress);
         pcvGuardian.withdrawToSafeAddress(address(pcvDeposit), mintAmount);
 
         assertEq(underlyingToken.balanceOf(address(this)), mintAmount);
     }
 
-    function testPCVWithdrawToSafeAddressFailWhenNotWhitelist() public {
-        vm.startPrank(addresses.governorAddress);
-        vm.expectRevert("Provided address is not whitelisted");
+    function testGuardianWithdrawToSafeAddress() public {
+        vm.prank(addresses.guardianAddress);
+        pcvGuardian.withdrawToSafeAddress(address(pcvDeposit), mintAmount);
+
+        assertEq(underlyingToken.balanceOf(address(this)), mintAmount);
+    }
+
+    function testPCVGuardWithdrawToSafeAddress() public {
+        vm.prank(guard);
+        pcvGuardian.withdrawToSafeAddress(address(pcvDeposit), mintAmount);
+
+        assertEq(underlyingToken.balanceOf(address(this)), mintAmount);
+    }
+
+    function testWithdrawToSafeAddressFailWhenNoRole() public {
+        vm.expectRevert(bytes("UNAUTHORIZED"));
+        pcvGuardian.withdrawToSafeAddress(address(pcvDeposit), mintAmount);
+    }
+
+    function testWithdrawToSafeAddressFailWhenNotWhitelist() public {
+        vm.prank(addresses.governorAddress);
+        vm.expectRevert(bytes("Provided address is not whitelisted"));
+
         pcvGuardian.withdrawToSafeAddress(address(0x1), mintAmount);
     }
 
-    function testPCVWithdrawToSafeAddressFailWhenNot() public {
-        vm.startPrank(addresses.governorAddress);
-        vm.expectRevert("Provided address is not whitelisted");
-        pcvGuardian.withdrawToSafeAddress(address(0x1), mintAmount);
+    function testSetWhiteListAddress() public {
+        vm.prank(addresses.governorAddress);
+
+        pcvGuardian.setWhitelistAddress(address(0x123));
+        assert(pcvGuardian.isWhitelistAddress(address(0x123)));
+    }
+
+    function testUnsetWhiteListAddress() public {
+        vm.prank(addresses.governorAddress);
+
+        pcvGuardian.unsetWhitelistAddress(address(pcvDeposit));
+        assert(!pcvGuardian.isWhitelistAddress(address(pcvDeposit)));
     }
 }
