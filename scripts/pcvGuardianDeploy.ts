07ecef14b515173809bd99aa391ec571e4a667ac
kassim <alkassimk@gmail.com>
2022-05-06 05:59:11 +0100

    complete deployment script

--- scripts/pcvGuardianDeploy.ts
+++ scripts/pcvGuardianDeploy.ts
@@ -5,17 +5,18 @@ import { keccak256 } from 'ethers/lib/utils';
 import config from './config';
 import { Core } from '@custom-types/contracts';
 
-const { CORE, MULTISIG_ADDRESS } = config;
+const { CORE, PROTOCOL_MULTISIG_ADDRESS, VOLT_FUSE_PCV_DEPOSIT, PCV_DEPOSIT } = config;
 
 const PCV_GUARD_ROLE = keccak256(utils.toUtf8Bytes('PCV_GUARD_ROLE'));
 
-const pcvGuardAddress = ''; //TODO fill in intial PCV Guard role address if setting at deployment
+const pcvGuardAddress1 = '0xf8D0387538E8e03F3B4394dA89f221D7565a28Ee';
+const pcvGuardAddress2 = '0xd90E9181B20D8D1B5034d9f5737804Da182039F6';
 
 async function deploy() {
   const PCVGuardian = await ethers.getContractFactory('PCVGuardian');
-  const whitelistAddresses = []; // TODO fill in initial whitelisted addresses
+  const whitelistAddresses = [VOLT_FUSE_PCV_DEPOSIT, PCV_DEPOSIT];
 
-  const pcvGuardian = await PCVGuardian.deploy(CORE, MULTISIG_ADDRESS, whitelistAddresses);
+  const pcvGuardian = await PCVGuardian.deploy(CORE, PROTOCOL_MULTISIG_ADDRESS, whitelistAddresses);
   await pcvGuardian.deployed();
 
   console.log('\n ~~~~~ Deployed PCV Guardian Successfully ~~~~~ \n');
@@ -27,13 +28,14 @@ async function deploy() {
   await core.grantGuardian(pcvGuardian.address);
 
   await core.createRole(PCV_GUARD_ROLE, await core.GOVERN_ROLE());
-  await core.grantRole(PCV_GUARD_ROLE, pcvGuardAddress);
+  await core.grantRole(PCV_GUARD_ROLE, pcvGuardAddress1);
+  await core.grantRole(PCV_GUARD_ROLE, pcvGuardAddress2);
 
   await verifyDeployment(core, pcvGuardian.address);
 
   await hre.run('verify:verify', {
     address: pcvGuardian.address,
-    constructorArguments: [CORE, MULTISIG_ADDRESS, whitelistAddresses]
+    constructorArguments: [CORE, PROTOCOL_MULTISIG_ADDRESS, whitelistAddresses]
   });
 
   return;
@@ -43,7 +45,8 @@ async function verifyDeployment(core: Core, pcvGuardian: string) {
   expect(await core.isPCVController(pcvGuardian)).to.be.true;
   expect(await core.isGuardian(pcvGuardian)).to.be.true;
 
-  expect(await core.hasRole(PCV_GUARD_ROLE, pcvGuardAddress)).to.be.true;
+  expect(await core.hasRole(PCV_GUARD_ROLE, pcvGuardAddress1)).to.be.true;
+  expect(await core.hasRole(PCV_GUARD_ROLE, pcvGuardAddress2)).to.be.true;
 }
 
 deploy()
