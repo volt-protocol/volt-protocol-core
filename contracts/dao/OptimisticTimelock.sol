babc8a6ad41c4790f920ce21365464d0a2fe49f9
Elliot <elliotfriedman3@gmail.com>
2022-06-06 11:40:17 -0700

    update comment in OA, upgrade to async 2.6.4

--- contracts/dao/OptimisticTimelock.sol
+++ contracts/dao/OptimisticTimelock.sol
@@ -20,7 +20,7 @@ contract OptimisticTimelock is TimelockController, CoreRef {
         @notice allow guardian or governor to assume timelock admin roles
         This more elegantly achieves optimistic timelock as follows:
         - veto: all proposers are granted the CANCELLER_ROLE in the constructor, 
-          which can cancel any in-flight proposal
+          CANCELLER_ROLE can cancel any in-flight proposal
         - pause proposals: revoke PROPOSER_ROLE from target
         - pause execution: revoke EXECUTOR_ROLE from target
         - set new proposer: revoke old proposer and add new one
