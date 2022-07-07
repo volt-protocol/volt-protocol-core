572cf225d26b98cf6b417fa3625cf9b8cf89106e
kassim <alkassimk@gmail.com>
2022-07-07 16:20:36 +0100

    BasePSM setup

---
+++ contracts/peg/BasePSM.sol
@@ -0,0 +1,394 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import "./../pcv/PCVDeposit.sol";
+import "./IBasePSM.sol";
+import "./../refs/OracleRef.sol";
+import "../Constants.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract BasePSM is IBasePSM, OracleRef, PCVDeposit, ReentrancyGuard {
+    using Decimal for Decimal.D256;
+    using SafeCast for *;
+    using SafeERC20 for IERC20;
+
+    /// @notice the amount of reserves to be held for redemptions
+    uint256 public override reservesThreshold;
+
+    /// @notice the PCV deposit target
+    IPCVDeposit public override surplusTarget;
+
+    /// @notice the token this PSM will exchange for VOLT
+    /// This token will be set to WETH9 if the bonding curve accepts eth
+    IERC20 public immutable override underlyingToken;
+
+    /// @notice boolean switch that indicates whether redemptions are paused
+    bool public redeemPaused;
+
+    /// @notice event that is emitted when redemptions are paused
+    event RedemptionsPaused(address account);
+
+    /// @notice event that is emitted when redemptions are unpaused
+    event RedemptionsUnpaused(address account);
+
+    /// @notice boolean switch that indicates whether minting is paused
+    bool public mintPaused;
+
+    /// @notice event that is emitted when minting is paused
+    event MintingPaused(address account);
+
+    /// @notice event that is emitted when minting is unpaused
+    event MintingUnpaused(address account);
+
+    /// @notice struct for passing constructor parameters related to OracleRef
+    struct OracleParams {
+        address coreAddress;
+        address oracleAddress;
+        address backupOracle;
+        int256 decimalsNormalizer;
+        bool doInvert;
+    }
+
+    /// @notice constructor
+    /// @param params PSM constructor parameter struct
+    constructor(
+        OracleParams memory params,
+        uint256 _reservesThreshold,
+        IERC20 _underlyingToken,
+        IPCVDeposit _surplusTarget
+    )
+        OracleRef(
+            params.coreAddress,
+            params.oracleAddress,
+            params.backupOracle,
+            params.decimalsNormalizer,
+            params.doInvert
+        )
+    {
+        underlyingToken = _underlyingToken;
+
+        _setReservesThreshold(_reservesThreshold);
+        _setSurplusTarget(_surplusTarget);
+        _setContractAdminRole(keccak256("PSM_ADMIN_ROLE"));
+    }
+
+    /// @notice modifier that allows execution when redemptions are not paused
+    modifier whileRedemptionsNotPaused() {
+        require(!redeemPaused, "PegStabilityModule: Redeem paused");
+        _;
+    }
+
+    /// @notice modifier that allows execution when minting is not paused
+    modifier whileMintingNotPaused() {
+        require(!mintPaused, "PegStabilityModule: Minting paused");
+        _;
+    }
+
+    /// @notice set secondary pausable methods to paused
+    function pauseRedeem() external onlyGovernorOrGuardianOrAdmin {
+        redeemPaused = true;
+        emit RedemptionsPaused(msg.sender);
+    }
+
+    /// @notice set secondary pausable methods to unpaused
+    function unpauseRedeem() external onlyGovernorOrGuardianOrAdmin {
+        redeemPaused = false;
+        emit RedemptionsUnpaused(msg.sender);
+    }
+
+    /// @notice set secondary pausable methods to paused
+    function pauseMint() external onlyGovernorOrGuardianOrAdmin {
+        mintPaused = true;
+        emit MintingPaused(msg.sender);
+    }
+
+    /// @notice set secondary pausable methods to unpaused
+    function unpauseMint() external onlyGovernorOrGuardianOrAdmin {
+        mintPaused = false;
+        emit MintingUnpaused(msg.sender);
+    }
+
+    /// @notice withdraw assets from PSM to an external address
+    function withdraw(address to, uint256 amount)
+        external
+        virtual
+        override
+        onlyPCVController
+    {
+        _withdrawERC20(address(underlyingToken), to, amount);
+    }
+
+    /// @notice set the ideal amount of reserves for the contract to hold for redemptions
+    function setReservesThreshold(uint256 newReservesThreshold)
+        external
+        override
+        onlyGovernorOrAdmin
+    {
+        _setReservesThreshold(newReservesThreshold);
+    }
+
+    /// @notice set the target for sending surplus reserves
+    function setSurplusTarget(IPCVDeposit newTarget)
+        external
+        override
+        onlyGovernorOrAdmin
+    {
+        _setSurplusTarget(newTarget);
+    }
+
+    /// @notice helper function to set reserves threshold
+    function _setReservesThreshold(uint256 newReservesThreshold) internal {
+        require(
+            newReservesThreshold > 0,
+            "PegStabilityModule: Invalid new reserves threshold"
+        );
+        uint256 oldReservesThreshold = reservesThreshold;
+        reservesThreshold = newReservesThreshold;
+
+        emit ReservesThresholdUpdate(
+            oldReservesThreshold,
+            newReservesThreshold
+        );
+    }
+
+    /// @notice helper function to set the surplus target
+    function _setSurplusTarget(IPCVDeposit newSurplusTarget) internal {
+        require(
+            address(newSurplusTarget) != address(0),
+            "PegStabilityModule: Invalid new surplus target"
+        );
+        IPCVDeposit oldTarget = surplusTarget;
+        surplusTarget = newSurplusTarget;
+
+        emit SurplusTargetUpdate(oldTarget, newSurplusTarget);
+    }
+
+    // ----------- Public State Changing API -----------
+
+    /// @notice send any surplus reserves to the PCV allocation
+    function allocateSurplus() external override {
+        int256 currentSurplus = reservesSurplus();
+        require(
+            currentSurplus > 0,
+            "PegStabilityModule: No surplus to allocate"
+        );
+
+        _allocate(currentSurplus.toUint256());
+    }
+
+    /// @notice function to receive ERC20 tokens from external contracts
+    function deposit() external override {
+        int256 currentSurplus = reservesSurplus();
+        if (currentSurplus > 0) {
+            _allocate(currentSurplus.toUint256());
+        }
+    }
+
+    /// @notice internal helper method to redeem VOLT in exchange for an external asset
+    function _redeem(
+        address to,
+        uint256 amountVoltIn,
+        uint256 minAmountOut
+    ) internal virtual returns (uint256 amountOut) {
+        amountOut = _getRedeemAmountOut(amountVoltIn);
+        require(
+            amountOut >= minAmountOut,
+            "PegStabilityModule: Redeem not enough out"
+        );
+
+        IERC20(volt()).safeTransferFrom(
+            msg.sender,
+            address(this),
+            amountVoltIn
+        );
+
+        _transfer(to, amountOut);
+
+        emit Redeem(to, amountVoltIn, amountOut);
+    }
+
+    /// @notice internal helper method to mint VOLT in exchange for an external asset
+    function _mint(
+        address to,
+        uint256 amountIn,
+        uint256 minAmountOut
+    ) internal virtual returns (uint256 amountVoltOut) {
+        amountVoltOut = _getMintAmountOut(amountIn);
+        require(
+            amountVoltOut >= minAmountOut,
+            "PegStabilityModule: Mint not enough out"
+        );
+
+        _transferFrom(msg.sender, address(this), amountIn);
+
+        uint256 amountVoltToTransfer = Math.min(
+            volt().balanceOf(address(this)),
+            amountVoltOut
+        );
+
+        if (amountVoltToTransfer != 0) {
+            IERC20(volt()).safeTransfer(to, amountVoltToTransfer);
+        }
+    }
+
+    /// @notice function to redeem VOLT for an underlying asset
+    function redeem(
+        address to,
+        uint256 amountVoltIn,
+        uint256 minAmountOut
+    )
+        external
+        virtual
+        override
+        nonReentrant
+        whenNotPaused
+        whileRedemptionsNotPaused
+        returns (uint256 amountOut)
+    {
+        amountOut = _redeem(to, amountVoltIn, minAmountOut);
+    }
+
+    /// @notice function to buy VOLT for an underlying asset
+    function mint(
+        address to,
+        uint256 amountIn,
+        uint256 minAmountOut
+    )
+        external
+        virtual
+        override
+        nonReentrant
+        whenNotPaused
+        whileMintingNotPaused
+        returns (uint256 amountVoltOut)
+    {
+        amountVoltOut = _mint(to, amountIn, minAmountOut);
+    }
+
+    // ----------- Public View-Only API ----------
+
+    /// @notice calculate the amount of VOLT out for a given `amountIn` of underlying
+    /// First get oracle price of token
+    /// Then figure out how many dollars that amount in is worth by multiplying price * amount.
+    /// ensure decimals are normalized if on underlying they are not 18
+    function getMintAmountOut(uint256 amountIn)
+        public
+        view
+        override
+        returns (uint256 amountVoltOut)
+    {
+        amountVoltOut = _getMintAmountOut(amountIn);
+    }
+
+    /// @notice calculate the amount of underlying out for a given `amountVoltIn` of VOLT
+    /// First get oracle price of token
+    /// Then figure out how many dollars that amount in is worth by multiplying price * amount.
+    /// ensure decimals are normalized if on underlying they are not 18
+    function getRedeemAmountOut(uint256 amountVoltIn)
+        public
+        view
+        override
+        returns (uint256 amountTokenOut)
+    {
+        amountTokenOut = _getRedeemAmountOut(amountVoltIn);
+    }
+
+    /// @notice the maximum mint amount out
+    function getMaxMintAmountOut() external view override returns (uint256) {
+        return volt().balanceOf(address(this));
+    }
+
+    /// @notice a flag for whether the current balance is above (true) or below (false) the reservesThreshold
+    function hasSurplus() external view override returns (bool) {
+        return balance() > reservesThreshold;
+    }
+
+    /// @notice an integer representing the positive surplus or negative deficit of contract balance vs reservesThreshold
+    function reservesSurplus() public view override returns (int256) {
+        return balance().toInt256() - reservesThreshold.toInt256();
+    }
+
+    /// @notice function from PCVDeposit that must be overriden
+    function balance() public view virtual override returns (uint256) {
+        return underlyingToken.balanceOf(address(this));
+    }
+
+    /// @notice returns address of token this contracts balance is reported in
+    function balanceReportedIn() public view override returns (address) {
+        return address(underlyingToken);
+    }
+
+    /// @notice override default behavior of not checking VOLT balance
+    function resistantBalanceAndVolt()
+        public
+        view
+        override
+        returns (uint256, uint256)
+    {
+        return (balance(), voltBalance());
+    }
+
+    // ----------- Internal Methods -----------
+
+    /// @notice helper function to get mint amount out based on current market prices
+    /// @dev will revert if price is outside of bounds and bounded PSM is being used
+    function _getMintAmountOut(uint256 amountIn)
+        internal
+        view
+        virtual
+        returns (uint256 amountVoltOut)
+    {
+        Decimal.D256 memory price = readOracle();
+        _validatePriceRange(price);
+
+        amountVoltOut = amountIn / price.value;
+    }
+
+    /// @notice helper function to get redeem amount out based on current market prices
+    /// @dev will revert if price is outside of bounds and bounded PSM is being used
+    function _getRedeemAmountOut(uint256 amountVoltIn)
+        internal
+        view
+        virtual
+        returns (uint256 amountTokenOut)
+    {
+        Decimal.D256 memory price = readOracle();
+        _validatePriceRange(price);
+
+        amountTokenOut = amountVoltIn * price.value;
+    }
+
+    /// @notice Allocates a portion of escrowed PCV to a target PCV deposit
+    function _allocate(uint256 amount) internal virtual {
+        _transfer(address(surplusTarget), amount);
+
+        surplusTarget.deposit();
+
+        emit AllocateSurplus(msg.sender, amount);
+    }
+
+    /// @notice transfer ERC20 token
+    function _transfer(address to, uint256 amount) internal {
+        SafeERC20.safeTransfer(underlyingToken, to, amount);
+    }
+
+    /// @notice transfer assets from user to this contract
+    function _transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        SafeERC20.safeTransferFrom(underlyingToken, from, to, amount);
+    }
+
+    // ----------- Hooks -----------
+
+    /// @notice overriden function in the bounded PSM
+    function _validatePriceRange(Decimal.D256 memory price)
+        internal
+        view
+        virtual
+    {}
+}
