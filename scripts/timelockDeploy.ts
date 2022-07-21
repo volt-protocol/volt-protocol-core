99f43c14df2e20ba87f59739273d41c759ddbfd2
Elliot <elliotfriedman3@gmail.com>
2022-07-21 16:34:27 -0700

    updated timelock deployment script to no longer use env variables, do not grant msig admin role over timelock, simulation scripts point to new timelock, integration tests, remove optimistic timelock

--- scripts/timelockDeploy.ts
+++ scripts/timelockDeploy.ts
@@ -2,21 +2,11 @@ import hre, { ethers } from 'hardhat';
 import NetworksForVerification from '@protocol/networksForVerification';
 import { TimelockController } from '@custom-types/contracts';
 import { expect } from 'chai';
+import { getAllContractAddresses } from './utils/loadContracts';
 
 const oneDay = 24 * 60 * 60; /// 1 day timelock delay
 
-/// deploying the timelock on mainnet should look like this:
-///   MSIG=0xcBB83206698E8788F85EFbEeeCAd17e53366EBDf PROPOSERS=0xcBB83206698E8788F85EFbEeeCAd17e53366EBDf,0xA96D4a5c343d6fE141751399Fc230E9E8Ecb6fb6,0xB320e376Be6459421695F2b6B1E716AE4bc8129A,0xd90E9181B20D8D1B5034d9f5737804Da182039F6 EXECUTORS=0xcBB83206698E8788F85EFbEeeCAd17e53366EBDf \
-///   npx hardhat run --network mainnet scripts/timelockDeploy.ts
-
-/// deploying the timelock on arbitrum should look like this:
-///   MSIG=0x1A1075cef632624153176CCf19Ae0175953CF010 PROPOSERS=0x1A1075cef632624153176CCf19Ae0175953CF010,0xA96D4a5c343d6fE141751399Fc230E9E8Ecb6fb6,0xB320e376Be6459421695F2b6B1E716AE4bc8129A,0xd90E9181B20D8D1B5034d9f5737804Da182039F6 EXECUTORS=0x1A1075cef632624153176CCf19Ae0175953CF010 \
-///   npx hardhat run --network arbitrumOne scripts/timelockDeploy.ts
-
-/// Script does not hardcode msig, proposers or executors because this system will be deployed
-/// onto multiple networks where all of these addresses will change
-
-async function deploy(proposers: string[], executors: string[], multisig: string) {
+async function deploy(proposers: string[], executors: string[]) {
   const deployer = (await ethers.getSigners())[0];
   const timelock = await (await ethers.getContractFactory('TimelockController')).deploy(oneDay, proposers, executors);
   await timelock.deployed();
@@ -24,13 +14,10 @@ async function deploy(proposers: string[], executors: string[], multisig: string
   console.log(`\nTimelock deployed to: ${timelock.address}`);
 
   const TIMELOCK_ADMIN_ROLE = await timelock.TIMELOCK_ADMIN_ROLE();
-  let tx = await timelock.grantRole(TIMELOCK_ADMIN_ROLE, multisig);
-  await tx.wait(1);
 
-  tx = await timelock.renounceRole(TIMELOCK_ADMIN_ROLE, deployer.address);
-  await tx.wait(1);
+  await (await timelock.renounceRole(TIMELOCK_ADMIN_ROLE, deployer.address)).wait(1);
 
-  console.log('\nDeployer renounced admin role, and granted admin to multisig');
+  console.log('\nDeployer renounced admin role');
 
   return timelock;
 }
@@ -42,8 +29,9 @@ async function validate(timelock: TimelockController, proposers: string[], execu
   const EXECUTOR_ROLE = await timelock.EXECUTOR_ROLE();
   const deployer = (await ethers.getSigners())[0];
 
+  expect(await timelock.hasRole(TIMELOCK_ADMIN_ROLE, timelock.address)).to.be.true;
   expect(await timelock.hasRole(TIMELOCK_ADMIN_ROLE, deployer.address)).to.be.false;
-  expect(await timelock.hasRole(TIMELOCK_ADMIN_ROLE, multisig)).to.be.true;
+  expect(await timelock.hasRole(TIMELOCK_ADMIN_ROLE, multisig)).to.be.false;
 
   for (let i = 0; i < proposers.length; i++) {
     expect(await timelock.hasRole(PROPOSER_ROLE, proposers[i])).to.be.true;
@@ -67,19 +55,17 @@ async function verifyEtherscan(timelock: string, proposers: string[], executors:
 }
 
 async function main() {
-  const { EXECUTORS, PROPOSERS, MSIG } = process.env;
+  const network = hre.network.name;
+  const contractAddresses = getAllContractAddresses();
 
-  if (!EXECUTORS || !PROPOSERS || !MSIG) {
-    throw new Error('Proposer(s), Executor(s), or Multisig not set in environment variables');
-  }
+  const multisig =
+    network === 'mainnet' ? contractAddresses.protocolMultisig : contractAddresses.arbitrumProtocolMultisig;
+  const executors = [contractAddresses.pcvGuardEOA1, contractAddresses.pcvGuardEOA2, contractAddresses.pcvGuardEOA3];
+  const proposers = [multisig, ...executors];
 
-  /// if there is an array, break addresses into their respective elements
-  const proposers = PROPOSERS.split(',');
-  const executors = EXECUTORS.split(',');
+  const timelock = await deploy(proposers, executors);
 
-  const timelock = await deploy(proposers, executors, MSIG);
-
-  await validate(timelock, proposers, executors, MSIG);
+  await validate(timelock, proposers, executors, multisig);
 
   if (NetworksForVerification[hre.network.name]) {
     await verifyEtherscan(timelock.address, proposers, executors);
