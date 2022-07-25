d72c47b8c2799b9d6c0d1f2a5860edd45b2c4871
Elliot <elliotfriedman3@gmail.com>
2022-07-25 15:09:37 -0700

    remove unused IERC20, VOLT and VCON imports

--- contracts/test/integration/utils/AllArbitrumRoles.sol
+++ contracts/test/integration/utils/AllArbitrumRoles.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.4;
 
-import {Core, Vcon, Volt, IERC20, IVolt} from "../../../core/Core.sol";
+import {Core} from "../../../core/Core.sol";
 import {ArbitrumAddresses} from "../fixtures/ArbitrumAddresses.sol";
 import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {TribeRoles} from "../../../core/TribeRoles.sol";
--- contracts/test/integration/utils/AllMainnetRoles.sol
+++ contracts/test/integration/utils/AllMainnetRoles.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.4;
 
-import {Core, Vcon, Volt, IERC20, IVolt} from "../../../core/Core.sol";
+import {Core} from "../../../core/Core.sol";
 import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {TribeRoles} from "../../../core/TribeRoles.sol";
 import {DSTest} from "./../../unit/utils/DSTest.sol";
