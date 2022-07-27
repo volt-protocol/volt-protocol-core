015565f3bcd7e7db83962301185054a68f05ac14
kassim <alkassimk@gmail.com>
2022-07-27 21:26:52 +0100

    allow 1bps deviation

--- contracts/test/integration/IntegrationTestPriceBoundPSM.t.sol
+++ contracts/test/integration/IntegrationTestPriceBoundPSM.t.sol
@@ -149,12 +149,12 @@ contract IntegrationTestPriceBoundPSMTest is DSTest {
         assertApproxEq(
             psm.getRedeemAmountOut(amountVoltIn).toInt256(),
             vanillaPriceBoundPSM.getRedeemAmountOut(amountVoltIn).toInt256(),
-            0
+            1
         );
         assertApproxEq(
             psm.getRedeemAmountOut(amountVoltIn).toInt256(),
             amountOut.toInt256(),
-            0
+            1
         );
     }
 
