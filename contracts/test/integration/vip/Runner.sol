f7110a5ceebce7478ecbdd3c9324ecc805f00574
Elliot <elliotfriedman3@gmail.com>
2022-08-02 22:21:59 -0700

    VIP 5 - burn excess VOLT in deprecated timelock

--- contracts/test/integration/vip/Runner.sol
+++ contracts/test/integration/vip/Runner.sol
@@ -1,36 +1,37 @@
 pragma solidity =0.8.13;
 
-import {vip4} from "./vip4.sol";
+import {vip5} from "./vip5.sol";
 import {TimelockSimulation} from "../utils/TimelockSimulation.sol";
 import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {ArbitrumAddresses} from "../fixtures/ArbitrumAddresses.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 /// @dev test harness for running and simulating VOLT Improvement Proposals
-contract Runner is TimelockSimulation, vip4 {
+contract Runner is TimelockSimulation, vip5 {
     /// remove all function calls inside testProposal and don't inherit the VIP
     /// once the proposal is live and passed
     function testProposalMainnet() public {
         mainnetSetup();
         simulate(
             getMainnetProposal(),
-            TimelockController(payable(MainnetAddresses.TIMELOCK_CONTROLLER)),
+            /// this time execute on the deprecated Timelock instead of the regular timelock
+            TimelockController(payable(MainnetAddresses.VOLT_TIMELOCK)),
             MainnetAddresses.GOVERNOR,
-            MainnetAddresses.EOA_1,
+            MainnetAddresses.EOA_2,
             vm
         );
         mainnetValidate();
     }
 
     function testProposalArbitrum() public {
-        arbitrumSetup();
-        simulate(
-            getArbitrumProposal(),
-            TimelockController(payable(ArbitrumAddresses.TIMELOCK_CONTROLLER)),
-            ArbitrumAddresses.GOVERNOR,
-            ArbitrumAddresses.EOA_1,
-            vm
-        );
-        arbitrumValidate();
+        // arbitrumSetup();
+        // simulate(
+        //     getArbitrumProposal(),
+        //     TimelockController(payable(ArbitrumAddresses.TIMELOCK_CONTROLLER)),
+        //     ArbitrumAddresses.GOVERNOR,
+        //     ArbitrumAddresses.EOA_1,
+        //     vm
+        // );
+        // arbitrumValidate();
     }
 }
