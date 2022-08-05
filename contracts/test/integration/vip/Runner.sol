6b7c0f26b26cc68bad20b4615963b8e1d5e48db3
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2022-08-05 12:15:20 -0700

    Create framework to ensure newly added PCV Controllers are also set in the PCV Guardian whitelist, delete outdated integration tests (#98)

--- contracts/test/integration/vip/Runner.sol
+++ contracts/test/integration/vip/Runner.sol
@@ -1,14 +1,22 @@
 pragma solidity =0.8.13;
 
-// import {vipx} from "./vipx.sol";
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 import {TimelockSimulation} from "../utils/TimelockSimulation.sol";
 import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {ArbitrumAddresses} from "../fixtures/ArbitrumAddresses.sol";
-import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+import {PCVGuardian} from "./../../../pcv/PCVGuardian.sol";
 
 /// @dev test harness for running and simulating VOLT Improvement Proposals
 /// inherit the proposal to simulate
 contract Runner is TimelockSimulation {
+    /// @notice mainnet PCV Guardian
+    PCVGuardian private immutable mainnetPCVGuardian =
+        PCVGuardian(MainnetAddresses.PCV_GUARDIAN);
+
+    /// @notice arbitrum PCV Guardian
+    PCVGuardian private immutable arbitrumPCVGuardian =
+        PCVGuardian(ArbitrumAddresses.PCV_GUARDIAN);
+
     /// remove all function calls inside testProposal and don't inherit the VIP
     /// once the proposal is live and passed
     function testProposalMainnet() public {
@@ -16,6 +24,7 @@ contract Runner is TimelockSimulation {
         // simulate(
         //     getMainnetProposal(),
         //     TimelockController(payable(MainnetAddresses.TIMELOCK_CONTROLLER)),
+        //     mainnetPCVGuardian,
         //     MainnetAddresses.GOVERNOR,
         //     MainnetAddresses.EOA_1,
         //     vm,
@@ -29,6 +38,7 @@ contract Runner is TimelockSimulation {
         // simulate(
         //     getArbitrumProposal(),
         //     TimelockController(payable(ArbitrumAddresses.TIMELOCK_CONTROLLER)),
+        //     arbitrumPCVGuardian,
         //     ArbitrumAddresses.GOVERNOR,
         //     ArbitrumAddresses.EOA_1,
         //     vm,
