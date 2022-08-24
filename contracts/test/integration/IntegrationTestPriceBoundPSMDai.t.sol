7f3f093e0d472c4c917509b23a6998a7824a9148
kassim <alkassimk@gmail.com>
2022-08-24 08:13:29 +0100

    remove maxmint test

--- contracts/test/integration/IntegrationTestPriceBoundPSMDai.t.sol
+++ contracts/test/integration/IntegrationTestPriceBoundPSMDai.t.sol
@@ -108,22 +108,6 @@ contract IntegrationTestPriceBoundPSMDaiTest is TimelockSimulation, vip7 {
     }
 
     /// @notice PSM is set up correctly and view functions are working
-    function testGetMaxMintAmountOut() public {
-        uint256 startingBalance = volt.balanceOf(address(psm));
-        assertEq(psm.getMaxMintAmountOut(), bufferCap + startingBalance);
-
-        vm.startPrank(MainnetAddresses.GOVERNOR);
-        core.grantMinter(MainnetAddresses.GOVERNOR);
-        volt.mint(address(psm), mintAmount);
-        vm.stopPrank();
-
-        assertEq(
-            psm.getMaxMintAmountOut(),
-            bufferCap + mintAmount + startingBalance
-        );
-    }
-
-    /// @notice PSM is set up correctly and view functions are working
     function testGetMintAmountOut(uint256 amountDaiIn) public {
         vm.assume(dai.balanceOf(address(this)) > amountDaiIn);
         uint256 currentPegPrice = oracle.getCurrentOraclePrice();
