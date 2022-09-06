394ce617f6659161b6107d66e2fe5ad0fca29db2
Elliot <elliotfriedman3@gmail.com>
2022-09-05 22:43:06 -0700

    remove requirement to specify max-old-space-size for node

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -21,12 +21,6 @@ const mainnetAlchemyApiKey = process.env.MAINNET_ALCHEMY_API_KEY;
 const arbitrumAlchemyApiKey = process.env.ARBITRUM_ALCHEMY_API_KEY;
 const useJSONTestReporter = process.env.REPORT_TEST_RESULTS_AS_JSON;
 
-if (!(process.env.NODE_OPTIONS && process.env.NODE_OPTIONS.includes('max-old-space-size'))) {
-  throw new Error(
-    `Please export node env var max-old-space-size before running hardhat. "export NODE_OPTIONS=--max-old-space-size=4096"`
-  );
-}
-
 if (enableMainnetForking) {
   if (!mainnetAlchemyApiKey) {
     throw new Error('Cannot fork mainnet without mainnet alchemy api key.');
