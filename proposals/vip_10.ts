08f469cdd03a4afba542b5943316d9e12e0c5bf5
Elliot <elliotfriedman3@gmail.com>
2022-09-08 00:40:54 -0700

    remove pesky hardcoded hardhat mainnet block number, vip 10 in typescript and deploy

---
+++ proposals/vip_10.ts
@@ -0,0 +1,31 @@
+import { ProposalDescription } from '@custom-types/types';
+
+const vip_10: ProposalDescription = {
+  title: 'VIP-10: ERC20 Allocator Deployment',
+  commands: [
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'createDeposit(address,address,uint248,int8)',
+      arguments: ['{usdcPriceBoundPSM}', '{usdcCompoundPCVDeposit}', '100000000000', '12'],
+      description: 'Add USDC deposit to the ERC20 Allocator'
+    },
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'createDeposit(address,address,uint248,int8)',
+      arguments: ['{daiPriceBoundPSM}', '{daiCompoundPCVDeposit}', '100000000000000000000000', '0'],
+      description: 'Add DAI deposit to the ERC20 Allocator'
+    },
+    {
+      target: 'core',
+      values: '0',
+      method: 'grantPCVController(address)',
+      arguments: ['{erc20Allocator}'],
+      description: 'Grant PCV Controller to the erc20Allocator'
+    }
+  ],
+  description: 'Create ERC20Allocator, add DAI and USDC psm and compound pcv deposit to the allocator'
+};
+
+export default vip_10;
