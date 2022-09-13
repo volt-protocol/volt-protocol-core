ef529174c278705d32a99b6ad9ed0d1b5c892820
kassim <alkassimk@gmail.com>
2022-09-13 14:22:30 +0100

    remove CoreRef

--- contracts/volt/VoltMigrator.sol
+++ contracts/volt/VoltMigrator.sol
@@ -1,28 +1,22 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity =0.8.13;
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {CoreRef} from "../refs/CoreRef.sol";
 import {IVolt} from "./IVolt.sol";
 import {IVoltMigrator} from "./IVoltMigrator.sol";
-import {MainnetAddresses} from "../test/integration/fixtures/MainnetAddresses.sol";
 
 /// @title Volt Migrator
 /// @notice This contract is used to allow user to migrate from the old VOLT token
 /// to the new VOLT token that will be able to participate in the Volt Veto Module.
 /// users will deposit their old VOLT token which'll be burnt, and the new Volt token
-/// will be minted and transferred to them.
-contract VoltMigrator is CoreRef, IVoltMigrator {
-    using SafeERC20 for IERC20;
-
+/// will be minted to them.
+contract VoltMigrator is IVoltMigrator {
     IVolt constant oldVolt = IVolt(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
+    IVolt constant newVolt = IVolt(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
 
-    IERC20 constant newVolt =
-        IERC20(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
-
-    constructor(address core) CoreRef(core) {}
+    constructor() {}
 
     /// @notice function to exchange old VOLT for new VOLT
     /// @param amount the amount of old VOLT user wishes to exchange
@@ -30,8 +24,7 @@ contract VoltMigrator is CoreRef, IVoltMigrator {
         oldVolt.transferFrom(msg.sender, address(this), amount);
         oldVolt.burn(amount);
 
-        newVolt.mint(amount);
-        newVolt.safeTransfer(msg.sender, amount);
+        newVolt.mint(msg.sender, amount);
     }
 
     /// @notice function to exchange old VOLT for new VOLT
@@ -44,8 +37,6 @@ contract VoltMigrator is CoreRef, IVoltMigrator {
         );
 
         oldVolt.burn(amountToExchange);
-
-        newVolt.mint(amountToExchange);
-        newVolt.safeTransfer(msg.sender, amountToExchange);
+        newVolt.mint(msg.sender, amountToExchange);
     }
 }
