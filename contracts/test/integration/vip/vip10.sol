39896c45f1ec8702d504e62a13f721357159e1f7
Elliot <elliotfriedman3@gmail.com>
2022-09-13 13:43:17 -0700

    add assertions to vip10

--- contracts/test/integration/vip/vip10.sol
+++ contracts/test/integration/vip/vip10.sol
@@ -10,6 +10,7 @@ import {Core} from "../../../core/Core.sol";
 import {IVIP} from "./IVIP.sol";
 import {DSTest} from "./../../unit/utils/DSTest.sol";
 import {PCVGuardian} from "../../../pcv/PCVGuardian.sol";
+import {ERC20Allocator} from "../../../pcv/utils/ERC20Allocator.sol";
 import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {ArbitrumAddresses} from "../fixtures/ArbitrumAddresses.sol";
 import {PegStabilityModule} from "../../../peg/PegStabilityModule.sol";
@@ -25,6 +26,10 @@ contract vip10 is DSTest, IVIP {
     address private fei = MainnetAddresses.FEI;
     address private core = MainnetAddresses.CORE;
 
+    uint256 private constant bufferCap = 300_000e18;
+    uint256 private constant rateLimitPerSecond = 5.78e18;
+    uint256 private constant maxRateLimitPerSecond = 1_000e18;
+
     /// @notice target token balance for the DAI PSM to hold
     uint248 private constant targetBalanceDai = 100_000e18;
 
@@ -35,12 +40,17 @@ contract vip10 is DSTest, IVIP {
     /// and properly update the buffer in ERC20Allocator
     int8 private constant usdcDecimalNormalizer = 12;
 
+    /// @notice no scaling to do on DAI as decimals are 18
+    int8 private constant daiDecimalNormalizer = 0;
+
     ERC20CompoundPCVDeposit private daiDeposit =
         ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_DAI_PCV_DEPOSIT);
     ERC20CompoundPCVDeposit private feiDeposit =
         ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_FEI_PCV_DEPOSIT);
     ERC20CompoundPCVDeposit private usdcDeposit =
         ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_USDC_PCV_DEPOSIT);
+    ERC20Allocator private allocator =
+        ERC20Allocator(MainnetAddresses.ERC20ALLOCATOR);
 
     ITimelockSimulation.action[] private proposal;
 
@@ -66,7 +76,7 @@ contract vip10 is DSTest, IVIP {
                     "connectPSM(address,uint248,int8)",
                     MainnetAddresses.VOLT_DAI_PSM,
                     targetBalanceDai,
-                    0 /// no decimal normalization
+                    daiDecimalNormalizer
                 ),
                 description: "Add DAI PSM to the ERC20 Allocator"
             })
@@ -117,11 +127,50 @@ contract vip10 is DSTest, IVIP {
 
     function mainnetSetup() public override {}
 
-    /// assert erc20 allocator is pcv controller
-    /// assert erc20 allocator has compound psm and pcv deposit connected
-    /// assert erc20 allocator has dai psm and pcv deposit connected
-    /// assert decimal normalization and target balances are correct for both dai and usdc
-    function mainnetValidate() public override {}
+    function mainnetValidate() public override {
+        assertEq(allocator.buffer(), bufferCap);
+        assertEq(allocator.bufferCap(), bufferCap);
+        assertEq(allocator.MAX_RATE_LIMIT_PER_SECOND(), maxRateLimitPerSecond);
+        assertEq(allocator.rateLimitPerSecond(), rateLimitPerSecond);
+
+        /// assert erc20 allocator has usdc psm and pcv deposit connected
+        {
+            address psmAddress = allocator.pcvDepositToPSM(address(daiDeposit));
+            assertEq(psmAddress, MainnetAddresses.VOLT_DAI_PSM);
+        }
+        /// assert erc20 allocator has dai psm and pcv deposit connected
+        {
+            address psmAddress = allocator.pcvDepositToPSM(
+                address(usdcDeposit)
+            );
+            assertEq(psmAddress, MainnetAddresses.VOLT_USDC_PSM);
+        }
+
+        /// assert decimal normalization and target balances are correct for both dai and usdc
+        {
+            (
+                address psmToken,
+                uint248 psmTargetBalance,
+                int8 decimalsNormalizer
+            ) = allocator.allPSMs(address(MainnetAddresses.VOLT_USDC_PSM));
+            assertEq(psmToken, MainnetAddresses.USDC);
+            assertEq(psmTargetBalance, targetBalanceUsdc);
+            assertEq(decimalsNormalizer, usdcDecimalNormalizer);
+        }
+        {
+            (
+                address psmToken,
+                uint248 psmTargetBalance,
+                int8 decimalsNormalizer
+            ) = allocator.allPSMs(address(MainnetAddresses.VOLT_DAI_PSM));
+            assertEq(psmToken, MainnetAddresses.DAI);
+            assertEq(psmTargetBalance, targetBalanceDai);
+            assertEq(decimalsNormalizer, daiDecimalNormalizer);
+        }
+
+        /// assert erc20 allocator is pcv controller
+        assertTrue(Core(core).isPCVController(MainnetAddresses.ERC20ALLOCATOR));
+    }
 
     /// prevent errors by reverting on arbitrum proposal functions being called on this VIP
     function getArbitrumProposal()
