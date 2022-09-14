15d9824f91b6770ba99c12eed34885bec831484d
kassim <alkassimk@gmail.com>
2022-09-14 12:22:12 +0100

    burnFrom and transfer tokens in migrator, add sweep function

--- contracts/volt/VoltMigrator.sol
+++ contracts/volt/VoltMigrator.sol
@@ -1,9 +1,11 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity =0.8.13;
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {CoreRef} from "../refs/CoreRef.sol";
+import {Volt} from "./Volt.sol";
 import {IVolt} from "./IVolt.sol";
 import {IVoltMigrator} from "./IVoltMigrator.sol";
 
@@ -12,19 +14,22 @@ import {IVoltMigrator} from "./IVoltMigrator.sol";
 /// to the new VOLT token that will be able to participate in the Volt Veto Module.
 /// users will deposit their old VOLT token which'll be burnt, and the new Volt token
 /// will be minted to them.
-contract VoltMigrator is IVoltMigrator {
-    IVolt constant oldVolt = IVolt(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
-    IVolt constant newVolt = IVolt(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
+contract VoltMigrator is IVoltMigrator, CoreRef {
+    using SafeERC20 for IERC20;
+    // solhint-disable-next-line const-name-snakecase
+    Volt public constant oldVolt =
+        Volt(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
+    // solhint-disable-next-line const-name-snakecase
+    IVolt public constant newVolt =
+        IVolt(0x559eBC30b0E58a45Cc9fF573f77EF1e5eb1b3E18);
 
-    constructor() {}
+    constructor(address core) CoreRef(core) {}
 
     /// @notice function to exchange old VOLT for new VOLT
     /// @param amount the amount of old VOLT user wishes to exchange
     function exchange(uint256 amount) external {
-        oldVolt.transferFrom(msg.sender, address(this), amount);
-        oldVolt.burn(amount);
-
-        newVolt.mint(msg.sender, amount);
+        oldVolt.burnFrom(msg.sender, amount);
+        newVolt.transfer(msg.sender, amount);
     }
 
     /// @notice function to exchange old VOLT for new VOLT
@@ -36,7 +41,19 @@ contract VoltMigrator is IVoltMigrator {
             oldVolt.allowance(msg.sender, address(this))
         );
 
-        oldVolt.burn(amountToExchange);
-        newVolt.mint(msg.sender, amountToExchange);
+        oldVolt.burnFrom(msg.sender, amountToExchange);
+        newVolt.transfer(msg.sender, amountToExchange);
+    }
+
+    /// @notice sweep target token,
+    /// @param token to sweep
+    /// @param to recipient
+    /// @param amount of token to be sent
+    function sweep(
+        address token,
+        address to,
+        uint256 amount
+    ) external onlyGovernor {
+        IERC20(token).safeTransfer(to, amount);
     }
 }
