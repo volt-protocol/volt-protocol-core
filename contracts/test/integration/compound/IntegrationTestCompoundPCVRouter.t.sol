6828c67b65e52d5ca61699a6b09e9ae901c8f9ab
Elliot <elliotfriedman3@gmail.com>
2022-09-18 15:06:56 -0700

    Compound PCV Router integration tests

--- contracts/test/integration/compound/IntegrationTestCompoundPCVRouter.t.sol
+++ contracts/test/integration/compound/IntegrationTestCompoundPCVRouter.t.sol
@@ -4,14 +4,16 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import {Vm} from "../../unit/utils/Vm.sol";
-import {DSTest} from "../../unit/utils/DSTest.sol";
-import {CompoundPCVRouter} from "../../../pcv/compound/CompoundPCVRouter.sol";
-import {ERC20CompoundPCVDeposit} from "../../../pcv/compound/ERC20CompoundPCVDeposit.sol";
 import {Core} from "../../../core/Core.sol";
-import {TribeRoles} from "../../../core/TribeRoles.sol";
+import {stdError} from "../../unit/utils/StdLib.sol";
+import {DSTest} from "../../unit/utils/DSTest.sol";
 import {MockERC20} from "../../../mock/MockERC20.sol";
+import {TribeRoles} from "../../../core/TribeRoles.sol";
+import {PCVGuardian} from "../../../pcv/PCVGuardian.sol";
 import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {ArbitrumAddresses} from "../fixtures/ArbitrumAddresses.sol";
+import {CompoundPCVRouter} from "../../../pcv/compound/CompoundPCVRouter.sol";
+import {ERC20CompoundPCVDeposit} from "../../../pcv/compound/ERC20CompoundPCVDeposit.sol";
 
 contract CompoundPCVRouterIntegrationTest is DSTest {
     using SafeCast for *;
@@ -24,26 +26,19 @@ contract CompoundPCVRouterIntegrationTest is DSTest {
     address private governor = MainnetAddresses.GOVERNOR;
 
     CompoundPCVRouter private compoundRouter;
-    ERC20CompoundPCVDeposit private daiDeposit;
-    ERC20CompoundPCVDeposit private usdcDeposit;
+    ERC20CompoundPCVDeposit private daiDeposit =
+        ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_DAI_PCV_DEPOSIT);
+    ERC20CompoundPCVDeposit private usdcDeposit =
+        ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_USDC_PCV_DEPOSIT);
 
-    uint256 public constant depositAmount = 10_000_000e18;
-    address public constant compoundPCVMover = address(1);
+    address public immutable pcvGuard = MainnetAddresses.EOA_1;
+    PCVGuardian public immutable pcvGuardian =
+        PCVGuardian(MainnetAddresses.PCV_GUARDIAN);
 
     /// @notice scaling factor for USDC
     uint256 public constant USDC_SCALING_FACTOR = 1e12;
 
     function setUp() public {
-        daiDeposit = new ERC20CompoundPCVDeposit(
-            address(core),
-            MainnetAddresses.CDAI
-        );
-
-        usdcDeposit = new ERC20CompoundPCVDeposit(
-            address(core),
-            MainnetAddresses.CUSDC
-        );
-
         compoundRouter = new CompoundPCVRouter(
             address(core),
             MainnetAddresses.MAKER_DAI_USDC_PSM,
@@ -51,37 +46,11 @@ contract CompoundPCVRouterIntegrationTest is DSTest {
             usdcDeposit
         );
 
-        /// role setup
         vm.prank(governor);
         core.grantPCVController(address(compoundRouter));
-
-        /// get funding for PCV Deposits
-        vm.startPrank(MainnetAddresses.DAI_USDC_USDT_CURVE_POOL);
-        dai.transfer(address(daiDeposit), depositAmount);
-        usdc.transfer(
-            address(usdcDeposit),
-            depositAmount / USDC_SCALING_FACTOR
-        );
-        vm.stopPrank();
-
-        daiDeposit.deposit();
-        usdcDeposit.deposit();
     }
 
     function testSetup() public {
-        /// cToken share price rounds down against depositors,
-        /// this means balance is a hair under deposit amount
-        assertApproxEq(
-            daiDeposit.balance().toInt256(),
-            depositAmount.toInt256(),
-            0
-        );
-        assertApproxEq(
-            usdcDeposit.balance().toInt256(),
-            (depositAmount / USDC_SCALING_FACTOR).toInt256(),
-            0
-        );
-
         assertTrue(core.isPCVController(address(compoundRouter)));
 
         assertEq(
@@ -97,45 +66,95 @@ contract CompoundPCVRouterIntegrationTest is DSTest {
 
     function testSwapDaiToUsdcSucceeds() public {
         uint256 withdrawAmount = daiDeposit.balance();
+        uint256 usdcStartingBalance = usdcDeposit.balance();
 
         vm.prank(governor);
         compoundRouter.swapDaiForUsdc(withdrawAmount); /// withdraw all balance
 
         assertApproxEq(
             usdcDeposit.balance().toInt256(),
-            ((withdrawAmount * 2) / USDC_SCALING_FACTOR).toInt256(),
+            (withdrawAmount / USDC_SCALING_FACTOR + usdcStartingBalance)
+                .toInt256(),
             0
         );
-        assertTrue(daiDeposit.balance() < 1e9); /// assert only dust remains
+        assertTrue(daiDeposit.balance() < 1e18); /// assert only dust remains
     }
 
     function testSwapUsdcToDaiSucceeds() public {
         uint256 withdrawAmount = usdcDeposit.balance();
+        uint256 daiStartingBalance = daiDeposit.balance();
 
         vm.prank(governor);
         compoundRouter.swapUsdcForDai(withdrawAmount); /// withdraw all balance
 
         assertApproxEq(
             daiDeposit.balance().toInt256(),
-            (depositAmount * 2).toInt256(),
+            (withdrawAmount * USDC_SCALING_FACTOR + daiStartingBalance)
+                .toInt256(),
             0
         );
-        assertTrue(usdcDeposit.balance() < 1e3); /// assert only dust remains
+        assertTrue(usdcDeposit.balance() < 1e6); /// assert only dust remains
     }
 
-    function testSwapUsdcToDaiSucceedsCompPCVMover() public {
-        vm.prank(compoundPCVMover);
+    function testSwapUsdcToDaiSucceedsPCVGuard() public {
+        uint256 withdrawAmount = usdcDeposit.balance();
+        uint256 daiStartingBalance = daiDeposit.balance();
+
+        vm.prank(pcvGuard);
+        compoundRouter.swapUsdcForDai(withdrawAmount); /// withdraw all balance
+
+        assertApproxEq(
+            daiDeposit.balance().toInt256(),
+            (withdrawAmount * USDC_SCALING_FACTOR + daiStartingBalance)
+                .toInt256(),
+            0
+        );
+        assertTrue(usdcDeposit.balance() < 1e6); /// assert only dust remains
     }
 
-    function testSwapDaiToUsdcSucceedsCompPCVMover() public {
-        vm.prank(compoundPCVMover);
+    function testSwapDaiToUsdcSucceedsPCVGuard() public {
+        uint256 withdrawAmount = daiDeposit.balance();
+        uint256 usdcStartingBalance = usdcDeposit.balance();
+
+        vm.prank(pcvGuard);
+        compoundRouter.swapDaiForUsdc(withdrawAmount); /// withdraw all balance
+
+        assertApproxEq(
+            usdcDeposit.balance().toInt256(),
+            (withdrawAmount / USDC_SCALING_FACTOR + usdcStartingBalance)
+                .toInt256(),
+            0
+        );
+        assertTrue(daiDeposit.balance() < 1e18); /// assert only dust remains
     }
 
-    function testSwapUsdcToDaiFailsNoLiquidity() public {}
+    function testSwapUsdcToDaiFailsNoLiquidity() public {
+        uint256 withdrawAmount = usdcDeposit.balance();
+        vm.startPrank(pcvGuard);
+        pcvGuardian.withdrawAllToSafeAddress(address(usdcDeposit)); /// pull all funds from usdc pcv deposit
+        vm.expectRevert("CompoundPCVDeposit: redeem error"); /// CUSDC has old cToken implementation that fails without reverting
+        compoundRouter.swapUsdcForDai(withdrawAmount); /// withdraw all balance
+        vm.stopPrank();
+    }
 
-    function testSwapDaiToUsdcFailsNoLiquidity() public {}
+    function testSwapDaiToUsdcFailsNoLiquidity() public {
+        uint256 withdrawAmount = daiDeposit.balance();
+        vm.startPrank(pcvGuard);
+        pcvGuardian.withdrawAllToSafeAddress(address(daiDeposit)); /// pull all funds from dai pcv deposit
+        vm.expectRevert(stdError.arithmeticError); /// CDAI has new cToken implementation that fails with a revert
+        compoundRouter.swapDaiForUsdc(withdrawAmount); /// withdraw all balance
+        vm.stopPrank();
+    }
 
-    function testSwapUsdcToDaiFailsUnauthorized() public {}
+    function testSwapUsdcToDaiFailsUnauthorized() public {
+        uint256 withdrawAmount = usdcDeposit.balance();
+        vm.expectRevert("UNAUTHORIZED");
+        compoundRouter.swapUsdcForDai(withdrawAmount);
+    }
 
-    function testSwapDaiToUsdcFailsUnauthorized() public {}
+    function testSwapDaiToUsdcFailsUnauthorized() public {
+        uint256 withdrawAmount = daiDeposit.balance();
+        vm.expectRevert("UNAUTHORIZED");
+        compoundRouter.swapDaiForUsdc(withdrawAmount);
+    }
 }
