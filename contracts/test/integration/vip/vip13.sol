02af9b2b02727219c41215a36129c84835ff2502
kassim <alkassimk@gmail.com>
2022-09-26 15:49:32 +0100

    complete solidity proposal

--- contracts/test/integration/vip/vip13.sol
+++ contracts/test/integration/vip/vip13.sol
@@ -14,11 +14,32 @@ import {MainnetAddresses} from "../fixtures/MainnetAddresses.sol";
 import {ArbitrumAddresses} from "../fixtures/ArbitrumAddresses.sol";
 import {PegStabilityModule} from "../../../peg/PegStabilityModule.sol";
 import {ITimelockSimulation} from "../utils/ITimelockSimulation.sol";
+import {ERC20Allocator} from "../../../pcv/utils/ERC20Allocator.sol";
 import {ERC20CompoundPCVDeposit} from "../../../pcv/compound/ERC20CompoundPCVDeposit.sol";
 
 contract vip13 is DSTest, IVIP {
     using SafeERC20 for IERC20;
     Vm public constant vm = Vm(HEVM_ADDRESS);
+    uint256 voltInUsdcPSM;
+    uint256 voltInDaiPSM;
+
+    ITimelockSimulation.action[] private proposal;
+
+    /// @notice target token balance for the DAI PSM to hold
+    uint248 private constant targetBalanceDai = 100_000e18;
+
+    /// @notice target token balance for the USDC PSM to hold
+    uint248 private constant targetBalanceUsdc = 100_000e6;
+
+    /// @notice scale up USDC value by 12 decimals in order to account for decimal delta
+    /// and properly update the buffer in ERC20Allocator
+    int8 private constant usdcDecimalNormalizer = 12;
+
+    /// @notice no scaling to do on DAI as decimals are 18
+    int8 private constant daiDecimalNormalizer = 0;
+
+    ERC20Allocator private allocator =
+        ERC20Allocator(MainnetAddresses.ERC20ALLOCATOR);
 
     ERC20CompoundPCVDeposit private daiDeposit =
         ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_DAI_PCV_DEPOSIT);
@@ -27,31 +48,187 @@ contract vip13 is DSTest, IVIP {
     ERC20CompoundPCVDeposit private usdcDeposit =
         ERC20CompoundPCVDeposit(MainnetAddresses.COMPOUND_USDC_PCV_DEPOSIT);
 
-    function getMainnetProposal()
-        public
-        pure
-        override
-        returns (ITimelockSimulation.action[] memory proposal)
-    {
-        proposal = new ITimelockSimulation.action[](1);
-
-        address[] memory toWhitelist = new address[](2);
-        // toWhitelist[0] = MainnetAddresses.NEW_VOLT_DAI_PSM;
-        // toWhitelist[1] = MainnetAddresses.NEW_VOLT_USDC_PSM;
-
-        proposal[0].target = MainnetAddresses.PCV_GUARDIAN;
-        proposal[0].value = 0;
-        proposal[0].arguments = abi.encodeWithSignature(
-            "addWhitelistAddresses(address[])",
-            toWhitelist
+    constructor() {
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.ERC20ALLOCATOR,
+                arguments: abi.encodeWithSignature(
+                    "disconnectPSM(address,uint248,int8)",
+                    MainnetAddresses.VOLT_USDC_PSM
+                ),
+                description: "Disconnect USDC PSM from the ERC20 Allocator"
+            })
         );
-        proposal[0]
-            .description = "Add new DAI, and USDC PSMs to PCV Guardian whitelist";
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.ERC20ALLOCATOR,
+                arguments: abi.encodeWithSignature(
+                    "connectPSM(address,uint248,int8)",
+                    MainnetAddresses.VOLT_DAI_PSM
+                ),
+                description: "Disconnect DAI PSM from the ERC20 Allocator"
+            })
+        );
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.ERC20ALLOCATOR,
+                arguments: abi.encodeWithSignature(
+                    "connectPSM(address,uint248,int8)",
+                    // MainnetAddresses.VOLTV2_USDC_PSM,
+                    targetBalanceUsdc,
+                    usdcDecimalNormalizer /// 12 decimals of normalization
+                ),
+                description: "Add USDC PSM to the ERC20 Allocator"
+            })
+        );
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.ERC20ALLOCATOR,
+                arguments: abi.encodeWithSignature(
+                    "connectPSM(address,uint248,int8)",
+                    // MainnetAddresses.VOLTV2_DAI_PSM,
+                    targetBalanceDai,
+                    daiDecimalNormalizer
+                ),
+                description: "Add DAI PSM to the ERC20 Allocator"
+            })
+        );
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.ERC20ALLOCATOR,
+                arguments: abi.encodeWithSignature(
+                    "connectDeposit(address,address)",
+                    // MainnetAddresses.VOLTV2_USDC_PSM,
+                    MainnetAddresses.COMPOUND_USDC_PCV_DEPOSIT
+                ),
+                description: "Connect USDC deposit to PSM in ERC20 Allocator"
+            })
+        );
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.ERC20ALLOCATOR,
+                arguments: abi.encodeWithSignature(
+                    "connectDeposit(address,address)",
+                    // MainnetAddresses.VOLTV2_DAI_PSM,
+                    MainnetAddresses.COMPOUND_DAI_PCV_DEPOSIT
+                ),
+                description: "Connect DAI deposit to PSM in ERC20 Allocator"
+            })
+        );
+
+        proposal.push(
+            ITimelockSimulation.action({
+                value: 0,
+                target: MainnetAddresses.PCV_GUARDIAN,
+                arguments: abi.encodeWithSignature(
+                    "addWhitelistAddresses(address[])"
+                    // [
+                    //     MainnetAddresses.VOLTV2_USDC_PSM,
+                    //     MainnetAddresses.VOLTV2_DAI_PSM
+                    // ]
+                ),
+                description: "Connect DAI deposit to PSM in ERC20 Allocator"
+            })
+        );
     }
 
-    function mainnetSetup() public override {}
+    function getMainnetProposal()
+        public
+        view
+        override
+        returns (ITimelockSimulation.action[] memory prop)
+    {
+        prop = proposal;
+    }
 
-    function mainnetValidate() public override {}
+    function mainnetSetup() public override {
+        // new pcv guardian to be deployed with safe address as timelock
+        uint256 governorVoltBalanceBeforeUsdc = IERC20(MainnetAddresses.VOLT)
+            .balanceOf(MainnetAddresses.GOVERNOR);
+
+        PCVGuardian(MainnetAddresses.PCV_GUARDIAN)
+            .withdrawAllERC20ToSafeAddress(
+                MainnetAddresses.VOLT_USDC_PSM,
+                MainnetAddresses.VOLT
+            );
+
+        uint256 governorVoltBalanceAfterUsdc = IERC20(MainnetAddresses.VOLT)
+            .balanceOf(MainnetAddresses.GOVERNOR);
+
+        voltInUsdcPSM =
+            governorVoltBalanceAfterUsdc -
+            governorVoltBalanceBeforeUsdc;
+
+        uint256 governorVoltBalanceBeforeDai = IERC20(MainnetAddresses.VOLT)
+            .balanceOf(MainnetAddresses.GOVERNOR);
+
+        PCVGuardian(MainnetAddresses.PCV_GUARDIAN)
+            .withdrawAllERC20ToSafeAddress(
+                MainnetAddresses.VOLT_DAI_PSM,
+                MainnetAddresses.VOLT
+            );
+
+        uint256 governorVoltBalanceAfterDai = IERC20(MainnetAddresses.VOLT)
+            .balanceOf(MainnetAddresses.GOVERNOR);
+
+        voltInDaiPSM =
+            governorVoltBalanceAfterDai -
+            governorVoltBalanceBeforeDai;
+    }
+
+    function mainnetValidate() public override {
+        // currently commented as new PSMs not deployed so will not compile
+        /// assert erc20 allocator has usdc psm and pcv deposit connected
+        // {
+        //     address psmAddress = allocator.pcvDepositToPSM(address(daiDeposit));
+        //     assertEq(psmAddress, MainnetAddresses.VOLTV2_DAI_PSM);
+        // }
+        // /// assert erc20 allocator has dai psm and pcv deposit connected
+        // {
+        //     address psmAddress = allocator.pcvDepositToPSM(
+        //         address(usdcDeposit)
+        //     );
+        //     assertEq(psmAddress, MainnetAddresses.VOLTV2_USDC_PSM);
+        // }
+        // /// assert decimal normalization and target balances are correct for both dai and usdc
+        // {
+        //     (
+        //         address psmToken,
+        //         uint248 psmTargetBalance,
+        //         int8 decimalsNormalizer
+        //     ) = allocator.allPSMs(address(MainnetAddresses.VOLTV2_USDC_PSM));
+        //     assertEq(psmToken, MainnetAddresses.USDC);
+        //     assertEq(psmTargetBalance, targetBalanceUsdc);
+        //     assertEq(decimalsNormalizer, usdcDecimalNormalizer);
+        // }
+        // {
+        //     (
+        //         address psmToken,
+        //         uint248 psmTargetBalance,
+        //         int8 decimalsNormalizer
+        //     ) = allocator.allPSMs(address(MainnetAddresses.VOLTV2_DAI_PSM));
+        //     assertEq(psmToken, MainnetAddresses.DAI);
+        //     assertEq(psmTargetBalance, targetBalanceDai);
+        //     assertEq(decimalsNormalizer, daiDecimalNormalizer);
+        // }
+        // /// assert that DAI PSM and USDC PSM are whitelisted in PCV Guardian
+        // assertTrue(
+        //     PCVGuardian(MainnetAddresses.PCV_GUARDIAN).isWhitelistAddress(
+        //         VOLTV2_USDC_PSM
+        //     )
+        // );
+        // assertTrue(
+        //     PCVGuardian(MainnetAddresses.PCV_GUARDIAN).isWhitelistAddress(
+        //         VOLTV2_DAI_PSM
+        //     )
+        // );
+    }
 
     /// prevent errors by reverting on arbitrum proposal functions being called on this VIP
     function getArbitrumProposal()
--- proposals/vip_13.ts
+++ proposals/vip_13.ts
@@ -4,10 +4,52 @@ const vip_13: ProposalDescription = {
   title: 'VIP-13: Add new PSMs to PCV Guardian Whitelist',
   commands: [
     {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'disconnectPSM(address)',
+      arguments: ['{usdcPriceBoundPSM}'],
+      description: 'Disconnet USDC PSM from the ERC20 Allocator'
+    },
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'disconnectPSM(address)',
+      arguments: ['{daiPriceBoundPSM}'],
+      description: 'Disconnet DAI PSM from the ERC20 Allocator'
+    },
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'connectPSM(address,uint248,int8)',
+      arguments: ['{voltV2UsdcPriceBoundPSM}', '100000000000', '12'],
+      description: 'Add USDC PSM to the ERC20 Allocator'
+    },
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'connectPSM(address,uint248,int8)',
+      arguments: ['{voltV2DaiPriceBoundPSM}', '100000000000000000000000', '0'],
+      description: 'Add DAI PSM to the ERC20 Allocator'
+    },
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'connectDeposit(address,address)',
+      arguments: ['{voltV2UsdcPriceBoundPSM}', '{usdcCompoundPCVDeposit}'],
+      description: 'Connect USDC deposit to PSM in ERC20 Allocator'
+    },
+    {
+      target: 'erc20Allocator',
+      values: '0',
+      method: 'connectDeposit(address,address)',
+      arguments: ['{voltV2daiPriceBoundPSM}', '{daiCompoundPCVDeposit}'],
+      description: 'Connect DAI deposit to PSM in ERC20 Allocator'
+    },
+    {
       target: 'pcvGuardian',
       values: '0',
       method: 'addWhitelistAddresses(address[])',
-      arguments: [['{voltV2DaiPriceBoundPSM}', '{voltV2UsdcPriceBoundPSM}']],
+      arguments: [['{voltV2UsdcPriceBoundPSM}', '{voltV2DaiPriceBoundPSM}']],
       description: 'Add new DAI, and USDC PSMs to PCV Guardian whitelist'
     }
   ],
