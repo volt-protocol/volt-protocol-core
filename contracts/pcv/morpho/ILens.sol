6ca9cc543ec64a68fa0734b906e6c1aa4bc6ebe3
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2022-10-18 13:14:31 -0400

    Update contracts/pcv/morpho/ILens.sol
    Co-authored-by: Romain Milon <rmilon@gmail.com>

--- contracts/pcv/morpho/ILens.sol
+++ contracts/pcv/morpho/ILens.sol
@@ -213,7 +213,11 @@ interface ILens {
     function getAverageBorrowRatePerBlock(address _poolToken)
         external
         view
-        returns (uint256);
+        returns (
+            uint256 avgBorrowRatePerBlock,
+            uint256 p2pBorrowAmount,
+            uint256 poolBorrowAmount
+        );
 
     function getRatesPerBlock(address _poolToken)
         external
