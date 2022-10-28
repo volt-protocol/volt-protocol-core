ca97a8f487a0ae9c82263570d24e604683a9b2c6
Elliot <elliotfriedman3@gmail.com>
2022-10-28 12:42:59 -0700

    add mock core ref v2

---
+++ contracts/mock/MockCoreRefV2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.13;
+
+import {CoreRefV2} from "../refs/CoreRefV2.sol";
+import {VoltRoles} from "./../core/VoltRoles.sol";
+
+contract MockCoreRefV2 is CoreRefV2 {
+    constructor(address core) CoreRefV2(core) {}
+
+    function testMinter() public onlyMinter {}
+
+    function testPCVController() public onlyPCVController {}
+
+    function testGovernor() public onlyGovernor {}
+
+    function testGuardian() public onlyGuardianOrGovernor {}
+
+    function testSystemState()
+        public
+        onlyVoltRole(VoltRoles.SYSTEM_STATE_ROLE)
+    {}
+}
