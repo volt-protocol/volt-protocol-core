70b2aba2dea2d028fde27bb5318206541cf72c5d
Elliot <elliotfriedman3@gmail.com>
2022-11-01 15:19:15 -0700

    Arbitrum Deployment Deprecated

--- contracts/test/integration/fixtures/ArbitrumAddresses.sol
+++ contracts/test/integration/fixtures/ArbitrumAddresses.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.13;
 
+/// @notice Arbitrum deployment is deprecated
+
 library ArbitrumAddresses {
     // ---------- VOLT ADDRESSES ----------
 
