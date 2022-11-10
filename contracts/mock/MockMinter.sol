a79bbb3959ee126e88865ab155e977339700aacb
Elliot <elliotfriedman3@gmail.com>
2022-11-10 01:10:28 -0800

    mock minter

---
+++ contracts/mock/MockMinter.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.13;
+
+import {IGRLM} from "../minter/IGRLM.sol";
+import {CoreRefV2} from "../refs/CoreRefV2.sol";
+
+contract MockMinter is CoreRefV2 {
+    IGRLM public grlm;
+
+    constructor(address core, address _grlm) CoreRefV2(core) {
+        grlm = IGRLM(_grlm);
+    }
+
+    function mint(address to, uint256 amount) external globalReentrancyLock {
+        grlm.mintVolt(to, amount);
+    }
+}
