7144a8b1bca3432506603ad5ecd3af09a9ecd5b1
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2022-11-14 12:36:48 -0800

    psm tests (#144)
    * start unit test
    
    * remove submodules
    
    * add core unit test

--- forge-std/src/Test.sol
+++ forge-std/src/Test.sol
@@ -2,7 +2,8 @@
 pragma solidity >=0.6.2 <0.9.0;
 
 import {CommonBase} from "./Common.sol";
-import "ds-test/test.sol";
+import {DSTest} from "./DSTest.sol";
+
 // forgefmt: disable-next-line
 import {console, console2, StdAssertions, StdCheats, stdError, stdJson, stdMath, StdStorage, stdStorage, StdUtils, Vm} from "./Components.sol";
 
