15bce0d56e6e81edeb4ec257b245c5eb04c918cf
Elliot <elliotfriedman3@gmail.com>
2022-11-29 18:27:02 -0800

    add assumption to differential test

--- contracts/test/integration/IntegrationTestCleanPriceBoundPSM.t.sol
+++ contracts/test/integration/IntegrationTestCleanPriceBoundPSM.t.sol
@@ -175,7 +175,7 @@ contract IntegrationTestCleanPriceBoundPSM is DSTest {
 
     /// @notice PSM is set up correctly and redeem view function is working
     function testGetRedeemAmountOutPpq(uint128 amountVoltIn) public {
-        vm.assume(amountVoltIn > 10_000_000); /// ensure accuracy down to the hundred thousandth
+        vm.assume(amountVoltIn > 1e10); /// ensure accuracy down to the hundred thousandth
 
         uint256 currentPegPrice = oracle.getCurrentOraclePrice() / 1e12;
 
