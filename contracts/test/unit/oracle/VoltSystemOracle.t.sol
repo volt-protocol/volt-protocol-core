33c47582846b3c02e89db47ada12d31ed3d752e5
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2022-12-08 09:48:35 -0800

    Add updateChangeRateBasisPoints function to VoltSystemOracle (#167)
    * Add updateChangeRateBasisPoints function to VoltSystemOracle to allow governor to update change rate
    
    * VoltSystemOracle add IOracleV2 compatability, optimize gas by using a single storage slot
    
    * update fixtures to compile with new VoltSystemOracle construction parameters
    
    * Update tests to work with updated Volt System Oracle
    
    * gas golf _compoundInterest

--- contracts/test/unit/oracle/VoltSystemOracle.t.sol
+++ contracts/test/unit/oracle/VoltSystemOracle.t.sol
@@ -6,30 +6,36 @@ import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {Vm} from "./../utils/Vm.sol";
 import {DSTest} from "./../utils/DSTest.sol";
-import {Decimal} from "./../../../external/Decimal.sol";
+import {CoreV2} from "../../../core/CoreV2.sol";
+import {getCoreV2} from "./../utils/Fixtures.sol";
 import {Constants} from "./../../../Constants.sol";
 import {VoltSystemOracle} from "../../../oracle/VoltSystemOracle.sol";
+import {TestAddresses as addresses} from "../utils/TestAddresses.sol";
 
 contract VoltSystemOracleUnitTest is DSTest {
-    using Decimal for Decimal.D256;
     using SafeCast for *;
+
+    CoreV2 private core;
 
     /// @notice reference to the volt system oracle
     VoltSystemOracle private voltSystemOracle;
 
     /// @notice increase the volt target price by 2% monthly
-    uint256 public constant monthlyChangeRateBasisPoints = 200;
+    uint16 public constant monthlyChangeRateBasisPoints = 200;
 
     /// @notice block time at which the VSO (Volt System Oracle) will start accruing interest
-    uint256 public constant startTime = 100_000;
+    uint40 public constant startTime = 100_000;
 
     /// @notice actual starting oracle price on mainnet
-    uint256 public constant startPrice = 1045095352308302897;
+    uint200 public constant startPrice = 1045095352308302897;
 
     Vm public constant vm = Vm(HEVM_ADDRESS);
 
     function setUp() public {
+        core = getCoreV2();
+
         voltSystemOracle = new VoltSystemOracle(
+            address(core),
             monthlyChangeRateBasisPoints,
             startTime,
             startPrice
@@ -121,8 +127,9 @@ contract VoltSystemOracleUnitTest is DSTest {
         voltSystemOracle.compoundInterest();
         vm.warp(block.timestamp + voltSystemOracle.TIMEFRAME());
 
-        (Decimal.D256 memory oraclePrice, ) = voltSystemOracle.read();
+        (uint256 oraclePrice, bool valid) = voltSystemOracle.read();
 
+        assertTrue(valid);
         assertEq(periodOneEndPrice, voltSystemOracle.oraclePrice());
         assertEq(
             previousStartTime + voltSystemOracle.TIMEFRAME(),
@@ -132,7 +139,7 @@ contract VoltSystemOracleUnitTest is DSTest {
             voltSystemOracle.getCurrentOraclePrice(),
             1087317204541558333 /// oracle value after 2 periods at 2% increase, compounded annually
         );
-        assertEq(voltSystemOracle.getCurrentOraclePrice(), oraclePrice.value);
+        assertEq(voltSystemOracle.getCurrentOraclePrice(), oraclePrice);
     }
 
     /// assert that price cannot increase before block.timestamp of 100,000
@@ -150,7 +157,7 @@ contract VoltSystemOracleUnitTest is DSTest {
     function testMultipleSequentialPeriodCompounds() public {
         /// anything over this amount of periods gets the oracle price into
         /// a zone where it could overflow during call to getCurrentOraclePrice
-        uint16 periods = 6192;
+        uint16 periods = 4500;
         vm.warp(
             voltSystemOracle.periodStartTime() +
                 (periods * voltSystemOracle.TIMEFRAME())
@@ -178,10 +185,12 @@ contract VoltSystemOracleUnitTest is DSTest {
         vm.warp(
             block.timestamp + voltSystemOracle.periodStartTime() + timeIncrease
         );
-        uint256 cachedOraclePrice = voltSystemOracle.oraclePrice();
 
-        (Decimal.D256 memory oraclePrice, ) = voltSystemOracle.read();
-        assertEq(voltSystemOracle.getCurrentOraclePrice(), oraclePrice.value);
+        uint256 cachedOraclePrice = voltSystemOracle.oraclePrice();
+        (uint256 oraclePrice, bool valid) = voltSystemOracle.read();
+
+        assertTrue(valid);
+        assertEq(voltSystemOracle.getCurrentOraclePrice(), oraclePrice);
         if (timeIncrease >= voltSystemOracle.TIMEFRAME()) {
             assertEq(
                 voltSystemOracle.getCurrentOraclePrice(),
@@ -344,23 +353,28 @@ contract VoltSystemOracleUnitTest is DSTest {
                 cachedOraclePrice;
             uint256 priceDelta = (priceChangeOverPeriod * timeDelta) /
                 voltSystemOracle.TIMEFRAME();
-            (Decimal.D256 memory oraclePrice, ) = voltSystemOracle.read();
+            (uint256 oraclePrice, bool valid) = voltSystemOracle.read();
 
+            assertTrue(valid);
             assertEq(
                 voltSystemOracle.getCurrentOraclePrice(),
                 priceDelta + cachedOraclePrice
             );
             assertEq(priceDelta + cachedOraclePrice, expectedOraclePrice);
-            assertEq(
-                voltSystemOracle.getCurrentOraclePrice(),
-                oraclePrice.value
-            );
+            assertEq(voltSystemOracle.getCurrentOraclePrice(), oraclePrice);
 
             bool isTimeEnded = block.timestamp >=
                 voltSystemOracle.periodStartTime() + duration;
 
             if (isTimeEnded) {
-                voltSystemOracle.compoundInterest();
+                if (periods % 2 == 0) {
+                    voltSystemOracle.compoundInterest();
+                } else {
+                    testGovernorUpdateChangeRateSucceeds(
+                        uint16(voltSystemOracle.monthlyChangeRateBasisPoints())
+                    );
+                }
+
                 assertEq(
                     periodStartTime + voltSystemOracle.TIMEFRAME(),
                     voltSystemOracle.periodStartTime()
@@ -377,6 +391,23 @@ contract VoltSystemOracleUnitTest is DSTest {
                 );
             }
         }
+    }
+
+    function testNonGovernorUpdateChangeRateFails() public {
+        vm.expectRevert("CoreRef: Caller is not a governor");
+        voltSystemOracle.updateChangeRateBasisPoints(0);
+    }
+
+    function testGovernorUpdateChangeRateSucceeds(uint16 newChangeRate) public {
+        vm.assume(newChangeRate <= 10_000);
+
+        vm.prank(addresses.governorAddress);
+        voltSystemOracle.updateChangeRateBasisPoints(newChangeRate);
+
+        assertEq(
+            voltSystemOracle.monthlyChangeRateBasisPoints(),
+            newChangeRate
+        );
     }
 
     /// Linear Interpolation Formula
