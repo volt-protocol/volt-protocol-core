2e4f1cef8170546dbe73694b2b05baaccdfe25f2
Erwan Beauvois <eswak@users.noreply.github.com>
2022-12-19 14:37:02 +0100

    Update contracts/deployment/SystemV2DeployScript.s.sol

--- contracts/deployment/SystemV2DeployScript.s.sol
+++ contracts/deployment/SystemV2DeployScript.s.sol
@@ -46,7 +46,6 @@ contract SystemV2DeployScript is Script, SystemV2 {
         console.log("PCV_ORACLE =", address(pcvOracle));
         console.log("ORACLE_CONSTANT_DAI =", address(daiConstantOracle));
         console.log("ORACLE_CONSTANT_USDC =", address(usdcConstantOracle));
-        console.log("ORACLE_CONSTANT_USDC =", address(usdcConstantOracle));
         console.log("VOLT_MIGRATOR =", address(voltMigrator));
         console.log("MIGRATOR_ROUTER =", address(migratorRouter));
     }
