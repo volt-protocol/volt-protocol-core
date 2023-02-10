9f7cd7730c86d1c3dd8ef9be6dc8d6afa25d94ed
Elliot <elliotfriedman3@gmail.com>
2023-02-09 18:07:16 -0800

    add lock tests

--- test/mock/MockCoreRefV2.sol
+++ test/mock/MockCoreRefV2.sol
@@ -17,6 +17,17 @@ contract MockCoreRefV2 is CoreRefV2 {
 
     function testSystemState() public onlyVoltRole(VoltRoles.LOCKER) {}
 
+    function testSystemLocksToLevel1() public globalLock(1) {}
+
+    function testSystemLocksToLevel2() public globalLock(2) {}
+
+    /// invalid lock level, doesn't matter because the lock is disabled in test
+    function testSystemLocksToLevel3() public globalLock(3) {}
+
+    function testSystemLockLevel1() public isGlobalReentrancyLocked(1) {}
+
+    function testSystemLockLevel2() public isGlobalReentrancyLocked(2) {}
+
     function testStateGovernorMinter()
         public
         hasAnyOfThreeRoles(
